d09ae7084483b91d368880ebaf4a38a1638039bf
thaovan26022001 <92176475+thaovan26022001@users.noreply.github.com>
2022-11-30 17:43:24 +0700

    complete header on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,9 @@ const modal = document.querySelector('.js-modal')
 const closes = document.querySelectorAll('.js-close')
 function showMenu() {
     modal.classList.add('header__modal-open')
-
 }
 function hideMenu() {
     modal.classList.remove('header__modal-open')
-
 }
 for (const actionMenu of actionMenus) {
     actionMenu.addEventListener('click', showMenu)
@@ -17,3 +15,17 @@ for ( const close of closes) {
 }
 
 
+const actionHeaders = document.querySelector('.js-action-1')
+const model = document.querySelector('.js-modal-1')
+const exits = document.querySelector('.js-close-1')
+function showHeader() {
+    model.classList.add('header__content-open')
+
+}
+function hideHeader() {
+    model.classList.remove('header__content-open')
+} 
+
+actionHeaders.addEventListener('click', showHeader)
+
+exits.addEventListener('click', hideHeader)
